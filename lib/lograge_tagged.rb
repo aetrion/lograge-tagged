a8bbafb95687ae020be528553b6117b8ca5d8a88
Simone Carletti <weppos@weppos.net>
2013-11-15 12:42:52 +0100

    Upgrade to the new lograge syntax

--- lib/lograge_tagged.rb
+++ lib/lograge_tagged.rb
@@ -2,25 +2,16 @@ require 'lograge'
 require 'lograge_tagged/version'
 
 module LogrageTagged
-  module LogSubscriber
-    # A custom formatter for Lograge logs.
-    #
-    # This formatter delegates the log generation to +process_action_lograge+,
-    # but it also appends a custom prefix for lookup.
-    def process_action_tagged(*args)
-      output = process_action_lograge(*args)
-      "[request.app] #{output}"
+  class TaggedKeyValueFormatter < Lograge::Formatters::KeyValue
+    def call(data)
+      "[request.app] #{super}"
     end
   end
 
   class Railtie < Rails::Railtie
-    initializer "lograge-tagged", :before => :lograge do |app|
-      app.config.lograge.log_format = :tagged
-      app.config.lograge.custom_options = ->(event) {
-        { params: event.payload[:params] }
-      }
+    initializer "lograge-tagged", before: :lograge do |app|
+      app.config.lograge.formatter = LogrageTagged::TaggedKeyValueFormatter.new
+      app.config.lograge.custom_options = ->(event) { { params: event.payload[:params] } }
     end
   end if defined?(Rails)
 end
-
-Lograge::RequestLogSubscriber.send(:include, LogrageTagged::LogSubscriber)
--- spec/lograge_tagged_spec.rb
+++ spec/lograge_tagged_spec.rb
@@ -1,22 +1,5 @@
 require 'spec_helper'
 require 'lograge_tagged'
 
-describe LogrageTagged::LogSubscriber do
-
-  subject { LogrageLogSubscriber.new }
-  
-  LogrageLogSubscriber = Class.new do
-    include LogrageTagged::LogSubscriber
-    def process_action_lograge(payload)
-      payload.map { |k,v| "#{k}=#{v}" }.join(" ")
-    end
-  end
-
-  describe "#process_action_tagged" do
-    it "prepends the tag to the processed payload" do
-      line = subject.process_action_tagged(foo: "1", bar: "2")
-      line.should eq("[request.app] foo=1 bar=2")
-    end
-  end
-
+describe LogrageTagged do
 end
